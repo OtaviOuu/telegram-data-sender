a35a20e4ca05e4e2e6485ac9ab174790cb46a04e
OtaviOuu <conta.otaviou@gmail.com>
2025-01-18 00:41:17 -0300

    Refactor main function to improve content management and add JSON output for folder tags

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from tqdm import tqdm
 import dotenv
 import os
-
+import json
 
 dotenv.load_dotenv()
 
@@ -19,7 +19,9 @@ client = TelegramClient("session_name", credentials["api_id"], credentials["api_
 
 target_channel_id = -1002480588574
 
-path = Path("./testes/Pikuma")
+path = Path("./testes/Dominando o calc")
+
+content_map = {}
 
 
 async def add_tag_to_folder_title(folder_title):
@@ -43,25 +45,49 @@ async def create_progress_bar(file):
 
 
 async def send_doc(doc):
-    progress_bar, progress_callback = create_progress_bar(doc)
 
-    with progress_bar:
+    doc_name = doc.name
+
+    with tqdm(
+        total=doc.stat().st_size,
+        unit="B",
+        unit_scale=True,
+        desc=doc_name,
+    ) as progress_bar:
+
+        def progress_callback(current, total):
+            progress_bar.n = current
+            progress_bar.total = total
+            progress_bar.refresh()
+
         await client.send_file(
             target_channel_id,
             file=doc,
-            caption=doc.name,
+            caption=doc_name,
             progress_callback=progress_callback,
         )
 
 
 async def send_video(video):
-    progress_bar, progress_callback = create_progress_bar(video)
 
-    with progress_bar:
+    video_name = video.name
+
+    with tqdm(
+        total=video.stat().st_size,
+        unit="B",
+        unit_scale=True,
+        desc=video_name,
+    ) as progress_bar:
+
+        def progress_callback(current, total):
+            progress_bar.n = current
+            progress_bar.total = total
+            progress_bar.refresh()
+
         await client.send_file(
             target_channel_id,
             video,
-            caption=video.name,
+            caption=video_name,
             supports_streaming=True,
             progress_callback=progress_callback,
         )
@@ -86,20 +112,38 @@ async def manage_content(content_file):
             await extension_data["handler"](content_file)
 
 
-async def send():
+async def main():
+    video_index = 0
     sorted_files = sorted(path.iterdir(), key=lambda x: x.name.lower())
     for file in sorted_files:
         if file.is_dir():
-            dir_name = await add_tag_to_folder_title(file.name)
-            await client.send_message(target_channel_id, dir_name)
+            dir_name = file.name
+            # aaaaaaaaaaaaaaaaa
+            content_map[dir_name] = []
+            # await client.send_message(target_channel_id, dir_name)
 
             sorted_videos = sorted(file.iterdir(), key=lambda x: x.name.lower())
             for content_file in sorted_videos:
-                await manage_content(content_file)
+                video_tag = f"#V{video_index}"
+                content_map[dir_name].append(video_tag)
 
+                with open("content_map.txt", "w") as f:
+                    json.dump(content_map, f, indent=4, ensure_ascii=False)
+                # await manage_content(content_file)
+                video_index += 1
         elif file.is_file():
             await manage_content(file)
 
+    guide_message = ""
+    for folder, tags in content_map.items():
+        guide_message += f"""
+        {folder}
+        {', '.join(tags)}
+        """
+    with open("final.txt", "w") as f:
+        f.write(guide_message)
+    await client.send_message(target_channel_id, guide_message)
+
 
 with client:
-    client.loop.run_until_complete(send())
+    client.loop.run_until_complete(main())
